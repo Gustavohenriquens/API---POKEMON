ec0adb1e5b8864cc47bd0aae731ceacedd8a1147
Gustavo Henrique <Gustavoproject01@gmail.com>
2024-01-23 12:05:53 -0300

    Commit da Api-Pokemon

---
+++ Pokemon.API/Api_Pokemon.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon.API
+{
+    class Api_Pokemon
+    {
+        static void Main(string[] args)
+        {
+
+            var url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var results = response.Content.ReadAsStringAsync().Result;
+
+                var listPokemon = JsonConvert.DeserializeObject<Pokemon>(results);
+
+                foreach (var poke in listPokemon.Results)
+                {
+                    Console.WriteLine(poke.Name);
+                }
+            }
+        }
+    }
+}
---
+++ Pokemon.API/Pokemon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Pokemon.API
+{
+    public class Pokemon
+    {
+
+        [JsonPropertyName("count")]
+        public long Count { get; set; }
+
+        [JsonPropertyName("next")]
+        public object Next { get; set; }
+
+        [JsonPropertyName("previous")]
+        public object Previous { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<Result> Results { get; set; }
+
+
+        public partial class Result
+        {
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+
+            [JsonPropertyName("url")]
+            public Uri Url { get; set; }
+        }
+    }
+}
